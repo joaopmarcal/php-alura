563fbe2fad44ae10f571aaa66d610f112d1d7a7f
joao Paulo <joaopaulozangrande@gmail.com>
2020-11-27 16:07:35 -0300

    implementando Herança

--- objeto/banco.php
+++ objeto/banco.php
@@ -1,18 +1,25 @@
 <?php
 
   require_once "src/Conta.php";
+  require_once "src/Pessoa.php";
+  require_once "src/Endereco.php";
+  require_once "src/Funcionario.php";
   require_once "src/Titular.php";
   require_once "src/Cpf.php";
 
-  $joaoPaulo = new Titular(new Cpf('123.456.789-10'), 'João Paulo');
+  $funcionario = new Funcionario('Tesla', new CPF('123.456.789-13'), 'Desenvolvedor');
+  var_dump($funcionario);
+
+  $endereco = new Endereco("São Paulo","bairro","av aguia", "1000");
+  $joaoPaulo = new Titular(new Cpf('123.456.789-10'), 'João Paulo',$endereco);
   $conta = new Conta($joaoPaulo);
   var_dump($conta);
   echo $conta->recuperaCPFTitular() .PHP_EOL;
   echo $conta->recuperaNomeTitular() .PHP_EOL;
   echo $conta->getSaldo() .PHP_EOL;
 
-  $joao = new Titular(new Cpf('123.456.789-11'), 'João');
-  $contaDois = new Conta($joao);
+  $patricia = new Titular(new Cpf('123.456.789-11'), 'Patricia',$endereco);
+  $contaDois = new Conta($patricia);
   var_dump($conta);
   echo $contaDois->recuperaCPFTitular() .PHP_EOL;
   echo $contaDois->recuperaNomeTitular() .PHP_EOL;
@@ -20,20 +27,12 @@
 
   echo Conta::recuperaNumeroDeContas();
 
+  $enderecoDois = new Endereco("a","b","c", "d");
+  $joao = new Titular(new Cpf('123.456.789-12'), 'João',$enderecoDois);
+  $conta = new Conta($joao);
+  var_dump($conta);
+  echo $conta->recuperaCPFTitular() .PHP_EOL;
+  echo $conta->recuperaNomeTitular() .PHP_EOL;
+  echo $conta->getSaldo() .PHP_EOL;
+
   exit('fim do programa');
-
-
-  $conta->cpfTitular = '12345678910';
-  $conta->nomeTitular = 'João Paulo';
-  $conta->depositar(500);
-
-  var_dump($conta);
-
-  $contaDois = new Conta();
-  $contaDois->depositar(300);
-  var_dump($contaDois);
-
-  $conta->transferir(1200,$contaDois);
-
-  var_dump($conta);
-  var_dump($contaDois);
