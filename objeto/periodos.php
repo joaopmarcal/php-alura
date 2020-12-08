757e56dcb7d75910c5870310756ff5f597b11d58
João Paulo <joaopaulozangrande@gmail.com>
2020-12-08 20:00:24 -0300

    i study the function data_period

---
+++ objeto/periodos.php
@@ -0,0 +1,13 @@
+<?php
+
+$inicio = new DateTime('2022-11-21');
+
+$intervalo = new DateInterval('P4Y');
+
+$fim = new DateTime('2042-11-22');
+
+$periodo = new DatePeriod($inicio, $intervalo,$fim);
+
+foreach ($periodo as $data){
+    echo $data->format('d/m/Y') . PHP_EOL;
+}
